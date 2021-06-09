051cae1a148fe76e8c7628194b83e48477b891c0
Ingrid Oncken <ingrid.oncken@gmail.com>
2021-06-09 15:16:38 +0100

    1o1

--- src/Component/SingleMovie.jsx
+++ src/Component/SingleMovie.jsx
@@ -1,28 +1,31 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import {Modal, Button} from "react-bootstrap"
-import {useState} from "react"
-import CommentList from "./CommentList";
+import React from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Modal, Button } from "react-bootstrap"
+import { useState } from "react"
+import CommentList from "./CommentList"
+import { Link } from "react-router-dom"
 
 const SingleMovie = (props) => {
-const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false)
 
-const handleClose = () => setShow(false);
-const handleShow = () => setShow(true);
-console.log(props)
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+  console.log(props)
   return (
     <>
-      <img src={props.img} className="thumbnail" onClick={handleShow} />
+      <Link to={"/details/" + props.id}>
+        <img src={props.img} className="thumbnail" />
+      </Link>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>{props.title}</Modal.Title>
         </Modal.Header>
-        <Modal.Body> 
-          <CommentList movieId={props.id}/>
+        <Modal.Body>
+          <CommentList movieId={props.id} />
         </Modal.Body>
       </Modal>
     </>
-  );
-};
+  )
+}
 
-export default SingleMovie;
+export default SingleMovie
